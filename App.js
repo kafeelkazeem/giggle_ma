d1d1384c915c543448aa686baad729bfbf343014
kafeelkazeem <kafeelkazeem@gmail.com>
2024-09-01 17:47:21 +0100

    added the splash screen

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'red'}}>Welcome To GIGGLE!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'red'
   },
 });
---
+++ util/colors.js
@@ -0,0 +1,6 @@
+//Color Palletes
+export const darkBrown = '#AA5B27'
+export const lightBrown = '#DDA15E'
+export const milkWhite = '#FEFAE0'
+export const darkGreen = '#283618'
+export const lightGreen = '#606C38'
