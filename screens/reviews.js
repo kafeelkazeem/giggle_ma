6df86ac283c2e77d8b5b3dc697540f2a369b4690
kafeelkazeem <kafeelkazeem@gmail.com>
2024-12-05 15:15:54 +0100

    new update to review

---
+++ screens/reviews.js
@@ -0,0 +1,47 @@
+import { View, Text, FlatList } from 'react-native';
+import React, { useEffect } from 'react';
+import { Card } from 'react-native-paper';
+import tw from "twrnc";
+import CustomStarRating from '../components/starRating/starRating';
+import { lightBrown } from '../util/colors';
+import { getInitials } from '../util/helpers';
+
+const Reviews = ({ route, navigation }) => {
+  const { customerReviews, businessName } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: businessName, // Set header title dynamically
+    });
+  }, [navigation, businessName]);
+
+  // Render item for FlatList
+  const renderItem = ({ item }) => (
+    <View style={tw`w-full flex flex-row items-start gap-3 mb-4 p-1`}>
+      {/* Avatar */}
+      <View style={tw`w-10 h-10 bg-[${lightBrown}] rounded-full flex items-center justify-center`}>
+        <Text style={tw`text-white font-bold text-lg`}>{getInitials(item.customer.fullName)}</Text>
+      </View>
+      {/* Review Details */}
+      <View style={tw`flex-1`}>
+        <Text style={tw`text-base font-semibold text-gray-800`}>{item.customer.fullName}</Text>
+        <Text style={tw`text-sm text-gray-700 my-1`}>{item.review}</Text>
+        <CustomStarRating rating={item.rating} />
+      </View>
+    </View>
+  );
+
+  return (
+    <View style={tw`flex-1`}>
+      <Card style={tw`bg-white w-[100%] shadow-md rounded-lg p-3 mt-1`}>
+        <FlatList
+          data={customerReviews}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()} // Use a keyExtractor for unique keys
+        />
+      </Card>
+    </View>
+  );
+};
+
+export default Reviews;
