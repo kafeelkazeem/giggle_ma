cc8627ed063f71e340315c1f0deae02b7f4e58f2
kafeelkazeem <kafeelkazeem@gmail.com>
2024-12-12 19:09:03 +0100

    updating search page ui

--- screens/search.js
+++ screens/search.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { View } from "react-native";
-import { Appbar, Searchbar } from "react-native-paper";
+import { Text, View } from "react-native";
+import { Appbar, Divider, Searchbar } from "react-native-paper";
 import tw from 'twrnc';
 import { darkBrown, lightBrown } from "../util/colors";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import TheSearch from '../assets/svg/search.svg'
 
 const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -18,26 +20,27 @@ const SearchPage = () => {
     <View style={tw`flex-1 bg-white`}>
       {/* App Bar */}
       <Appbar.Header style={tw`bg-[${darkBrown}]`}>
-        <Appbar.Content title="Search Technicians" color="white" />
+        <Appbar.Content title={<Text style={tw`text-2xl text-white tracking-0.5 font-bold`}>Search <AntDesign name="search1" size={25} color="white" /></Text>} color="white" />
       </Appbar.Header>
-
       {/* Search Bar and Button Row */}
-      <View style={tw`flex-row items-center px-4 mt-4`}>
-      <Searchbar
-        placeholder="search by name, state, address...."
-        onChangeText={setSearchQuery}
-        value={searchQuery}
-        mode='bar'
-        theme={{ colors: { primary: lightBrown, placeholder: 'grey' } }}  
-        style={{
-          backgroundColor: "rgba(221, 161, 94, 0.2)", 
-          borderColor: 'grey', 
-          borderWidth: 1
-        }}
-        rippleColor={darkBrown}
-      />
+      <View style={tw`flex-row items-center px-3 mt-4`}>
+        <Searchbar
+          placeholder="search by name, address, profess....."
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+          mode='bar'
+          onSubmitEditing={() => alert('ih')}
+          theme={{ colors: { primary: lightBrown, placeholder: 'grey' } }}  
+          style={[tw`border py-1`,{backgroundColor: "rgba(221, 161, 94, 0.2)", borderRadius: 10}]}
+          rippleColor={darkBrown}
+        />
       </View>
-      {/* Rest of the content */}
+      <View style={tw`flex-1 p-2 px-4`}>
+        <Divider style={tw`mb-1 mt-4`} bold={true} />
+        <View style={tw`flex-1 w-full justify-center items-center`}>
+          <TheSearch />
+        </View>
+      </View>
     </View>
   );
 };
--- util/url.js
+++ util/url.js
@@ -1,1 +1,2 @@
 export const ApiUrl = 'https://giggle-be.onrender.com/api'
+export const localhostApiUrl = 'http://192.168.43.87:8080/api'
