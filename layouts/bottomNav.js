f325a8ed0c30f9dde061ca441cb0d21490353fb1
kafeelkazeem <kafeelkazeem@gmail.com>
2024-12-16 18:03:06 +0100

    new update to settings screen and bottom navigation

--- layouts/bottomNav.js
+++ layouts/bottomNav.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { BottomNavigation } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import { Alert, BackHandler, StyleSheet } from 'react-native';
 import SettingsPage from '../screens/settings';
 import MapPage from '../screens/map';
 import SearchPage from '../screens/search';
 import HomePage from '../screens/home';
 import { darkBrown } from '../util/colors';
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 const HomePageRoute = () => <HomePage /> ;
 
@@ -17,6 +18,41 @@ const SettingsPageRoute = () => <SettingsPage />;
 
 
 const BottomNav = () => {
+
+  const navigation = useNavigation()
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const handleBackPress = () => {
+        Alert.alert(
+          "Logout",
+          "Are you sure you want to Logout?",
+          [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Exit cancelled"),
+              style: "cancel",
+            },
+            {
+              text: "Logout",
+              onPress: () => navigation.navigate('Signin'),
+              style: "destructive",
+            },
+          ],
+          { cancelable: true }
+        );
+        return true; // Prevents the default back button behavior
+      };
+
+      // Add the back button listener
+      BackHandler.addEventListener("hardwareBackPress", handleBackPress);
+
+      // Cleanup the listener when the Home Page loses focus
+      return () =>
+        BackHandler.removeEventListener("hardwareBackPress", handleBackPress);
+    }, [])
+  );
+
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
     { key: 'home', title: 'Home', focusedIcon: 'home'},
--- screens/settings.js
+++ screens/settings.js
@@ -1,16 +1,96 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { Appbar } from "react-native-paper";
+import { FlatList, Text, TouchableOpacity, View, Alert } from "react-native";
+import { Appbar, Divider } from "react-native-paper";
 import tw from 'twrnc';
-import { darkBrown } from "../util/colors";
+import { darkBrown, lightBrown } from "../util/colors";
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useNavigation } from "@react-navigation/native";
+
+const logout = () =>{
+    Alert.alert(
+        "Log Out", // Title of the alert
+        "Are you sure you want to log out?", // Message in the alert
+        [
+          {
+            text: "Cancel",
+            
+            style: "cancel", // Styles the button (e.g., "cancel", "destructive")
+          },
+          {
+            text: "Log Out",
+            onPress: () => console.log('logout'),
+            style: "destructive", // Typically for actions like deletion or logging out
+          },
+        ],
+        { cancelable: true } // Allows dismissal by tapping outside the alert
+      );
+}
+
+const content = [
+    {
+        title: 'Profile',
+        icon: <Entypo name="user" size={25} color='black' />,
+        action: ''
+    },
+    {
+        title: 'Change Password',
+        icon: <Entypo name="key" size={25} color='black' />,
+        action: ''
+    },
+    {
+        title: 'Search Range',
+        icon: <FontAwesome name="map-marker" size={25} color='black' />,
+        action: ''
+    },
+    {
+        title: 'Terms and Condition',
+        icon: <FontAwesome name="legal" size={25} color='black' />,
+        action: ''
+    },
+    {
+        title: 'Logout',
+        icon: <MaterialIcons name="logout" size={25} color='black' />,
+        action: logout
+    }
+]
+
+const ButtonList = ({title, icon, handlePress}) => {
+    return (
+      <>
+      <View style={tw`p-3 py-5 flex justify-center items-center mt-0`}>  
+      <TouchableOpacity onPress={handlePress} style={tw`flex flex-row items-center p-4 rounded`} activeOpacity={0.8}>
+         {icon}
+        <View style={tw`flex-1 ml-10`}>
+          <Text style={tw`text-base font-medium tracking-0.2`}>{title}</Text>
+        </View>
+        <MaterialCommunityIcons name="greater-than" size={24} color={darkBrown} />
+      </TouchableOpacity>
+      </View>
+      <Divider />
+      </>
+    );
+  };
 
 const SettingsPage = () =>{
     return(
         <View style={tw`flex-1 bg-white`}>
             <Appbar.Header style={tw`bg-[${darkBrown}]`}>
-                <Appbar.Content title={<Ionicons name="settings-sharp" size={27} color="white" />} color="white" />
+                <Appbar.Content title={<Text style={tw`text-2xl text-white tracking-0.5 font-bold`}><Ionicons name="settings-sharp" size={27} color="white" /></Text>} color="white" />
             </Appbar.Header>
+            <View style={tw`w-full p-4`}>
+                <Text style={tw`text-2xl font-bold tracking-0.5`}>General</Text>
+            </View>
+            <View style={tw`flex-1 bg-white w-full -mt-5`}>
+                <FlatList
+                data={content}
+                renderItem={({ item }) => <ButtonList title={item.title} icon={item.icon} handlePress={item.action} />}
+                keyExtractor={(item, index) => index.toString()}
+                />
+            </View>
         </View>
     )
 }
